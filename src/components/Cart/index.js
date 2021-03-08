18681d6b86890daaefec4412542bb6ae6a999b4e
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2021-03-08 16:36:27 -0500

    March 8, 2021 4:36 PM

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -4,7 +4,7 @@ import React from "react"
 export default class Cart extends React.Component {
     render() {
         return(
-            <p>You have {this.state.value} items in the cart</p>
+            <p>You have 1 items in the cart</p>
         )
     }
 }

4cef2c6a133fdb38d463937c240f535e7610f7c0
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2021-03-08 14:11:33 -0500

    first commit

---
+++ src/components/MenuItem/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default class MenuItem extends React.Component {
+  render() {
+      return (<div className="menu-item" id={`menu-item-${this.props.itemId}`}>
+              <span>${this.props.itemPrice }</span>
+              <h2>{this.props.itemName}</h2>
+              <p>{this.props.itemDescription}</p>
+          <button>Add to Cart</button>
+          </div>
+    );
+  }
+}

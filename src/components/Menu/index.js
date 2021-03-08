8270bdf0d587704d29bd9bd0a3abe386f7a88951
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2021-03-08 14:59:47 -0500

    March 8, 2021 2:59 PM

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,10 +2,16 @@ import React from "react"
 import MenuItem from "../MenuItem"
 
 export default class Menu extends React.Component {
+    h1style = {
+        color: 'white',
+        textAlign: 'center',
+        font: 'helvertica',
+    };
+
     render() {
         return (
             <div>
-                <h1>{this.props.menuName} Menu</h1>
+                <h1 style={this.h1style}>{this.props.menuName} Menu</h1>
                 {this.props.menuItems.map((menuId) => {
                     return (
                         <MenuItem {...menuId}></MenuItem>)

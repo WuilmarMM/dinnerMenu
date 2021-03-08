f5a3a2a316d9a0f3f002b141ed3109b3ed87dcf7
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2021-03-08 17:12:38 -0500

    March 8, 2021 5:12 PM

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -7,7 +7,7 @@ export default class MenuItem extends React.Component {
   }
 
   handleClick() {
-    this.props.MenuItem.setState((prevState) => {
+    this.setState((prevState) => {
       let newValue = prevState.value + 1
       this.props.updateValue(newValue)
       return {
